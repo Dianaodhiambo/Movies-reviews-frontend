fd5ff2784a82c3de6742c293e9a79b0d0cda8c87
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-12-08 11:41:59 +0300

    create rating compnent

---
+++ src/Components/Rating.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { FaStar} from "react-icons/fa";
+
+const Rating = ({value, text, color}) => {
+  return (
+    <div className='rating'>
+              <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 1
+              ? <FaStar/>
+              : value >= 0.5
+              ? 'FaStar-half-alt'
+              : <FaStar/>
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 2
+              ? <FaStar/>
+              : value >= 1.5
+              ? 'FaStar-half-alt'
+              : <FaStar/>
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 3
+              ? <FaStar/>
+              : value >= 2.5
+              ? 'FaStar-half-alt'
+              : <FaStar/>
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 4
+              ? <FaStar/>
+              : value >= 3.5
+              ? 'FaStar-half-alt'
+              : <FaStar/>
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          style={{ color }}
+          className={
+            value >= 5
+              ? <FaStar/>
+              : value >= 4.5
+              ? 'FaStar-half-alt'
+              : <FaStar/>
+          }
+        ></i>
+      </span>
+      <span>{text && text}</span>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+    color: '#f8e825',
+  }
+  
+  Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+  }
+
+export default Rating
