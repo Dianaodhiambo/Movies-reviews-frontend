f33eaf8d44878bf1304e3157c60164553166e5ef
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-12-08 12:34:50 +0300

    add Card Components to movies

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import Card from './Card'
+import Card from '../Components/Card'
 
 function MovieList () {
     const [movies, setMovies] = useState([])
