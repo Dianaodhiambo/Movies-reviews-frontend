59ef9bdf1cb8a9e7f09e572c088cbc826d1c00cb
diana nthenya odhiambo <dianaodhiambo1096@gmail.com>
2022-12-11 01:12:12 +0300

    make changes in submission

--- src/Components/CreateAccount.js
+++ src/Components/CreateAccount.js
@@ -1,67 +1,54 @@
 import React from 'react'
 import { useState} from "react";
+import { NavLink } from 'react-router-dom';
 
-function CreateAccount() {
-    const [username, setUserName] = useState("");
-    const [password, setPassword] =  useState ("")
-    const [message, setMessage] = useState("");
-
-  
-    let handleSubmit = async (e) => {
+function CreateAccount({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  function handleSubmit(e) {
       e.preventDefault();
-      try {
-        let res = await fetch("http://127.0.0.1:3000/api/v1/users", {
+      fetch("https://moviesreviews-production.up.railway.app/users",{
           method: "POST",
-          body: JSON.stringify({
-            username: username,
-            password: password,
-            
-          }),
-        });
-        
-        if (res.status === 200) {
-          setUserName("");
-          setPassword("");
-          setMessage("User created successfully");
-        } else {
-          setMessage("Some error occured");
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+      }).then((r) => {
+          if (r.ok) {
+              r.json().then((user) => onLogin(user));
+          } else {
+              r.json().then((err) => setErrors(err.errors));
+          }
+      });
+  }
 
   return (
-    <div id="register">
-        <div className="Register" style={{textAlign:"center", color:"#5A738E", fontWeight:"bold"}}>
-                <h2>Mo-vife</h2>
-                <span>Welcome to Mo-Vife!</span>
+      <form className='form' onSubmit={handleSubmit}>
+          <label className="inputs-form" htmlFor="username">Username</label>
+          <input
+              type="text"
+              id="username"
+              autoComplete="off"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+          />
+          <label className="inputs-form" htmlFor="password">Password</label>
+          <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+          />
+          <button id="sign-up" type="submit">
+              {"Login"}
+          </button>
+          <p>Or <NavLink className="" to='/signup'>create account</NavLink></p>
 
-                <div id="Column" align='center'>
-                <form id='register-form' onSubmit={handleSubmit} > 
-                    <input 
-                        type="text" 
-                        value={username}
-                        placeholder="Username"
-                        onChange={(e) => setUserName(e.target.value)}
-                        />
-
-                      <input 
-                        type="text" 
-                        value={password}
-                        placeholder="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        /> 
-
-                    <button className="btn">SignUp</button>
-
-                    <div className="message">{message ? <p>{message}</p> : null}</div>
-                </form>
-            </div>
-        </div>
-    </div>
-    
-  )
+          {errors.map((err) => (
+              <p style={{ color: "red" }} key={err}>{err}</p>
+          ))}
+      </form>
+  );
 }
-
 export default CreateAccount
